248d0baa50fb5be1953fe9fd777c9877ab5e3019
Sergei <tolulisergei@gmail.com>
2023-03-08 00:39:31 +0100

    Working on smart_function to give intelligent answers

--- app.py
+++ app.py
@@ -10,26 +10,25 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        prompt = request.form["prompt"]
+        response = smart_function(prompt)
+        return redirect(url_for("index", result=response))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def smart_function(prompt):
+    prompt = prompt
+    model = "davinci"
+    temperature = 0.5
+    max_tokens = 5          
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+    response = openai.Completion.create(
+    engine=model,
+    prompt=prompt,
+    temperature=temperature,
+    max_tokens=max_tokens
     )
+
+    return response.choices[0].text
